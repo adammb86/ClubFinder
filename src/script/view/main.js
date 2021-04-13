c6cce32b5ab08bff093a60cf2c5111c20515962d
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2021-04-13 08:44:26 +0700

    Ubah template literals clubElement.innerHTML di main.js

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -18,11 +18,12 @@ const main = function() {
             const clubElement = document.createElement("div");
             clubElement.setAttribute("class", "club");
 
-            clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-                '<div class="club-info">\n' +
-                '<h2>' + name + '</h2>\n' +
-                '<p>' + description + '</p>' +
-                '</div>';
+            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+                <div class="club-info">
+                    <h2>${name}</h2>
+                    <p>${description}</p>
+                </div>`
+
             clubListElement.appendChild(clubElement);
         })
     };
