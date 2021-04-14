05bc2d034c7f940d81dc230122779c9a8a3a9a81
adammb86 <adammbachtiar@gmail.com>
2021-04-14 12:42:59 +0700

    Hapus konstruktor di DataSource dalam rangka penerapan async-await

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,9 +1,5 @@
 class DataSource {
-    constructor(onSuccess, onFailed){
-        this.onSuccess = onSuccess;
-        this.onFailed = onFailed;
-    }  
-
+    
     searchClub(keyword) {
         const filteredClubs = clubs.filter(club => {
             return club.name.toUpperCase().includes(keyword.toUpperCase());
