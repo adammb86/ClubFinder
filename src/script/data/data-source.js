25675966852ceb5c3d58a2e2ec8aa657190f3d19
adammb86 <adammbachtiar@gmail.com>
2021-04-14 12:47:54 +0700

    Ubah if menggunakan resolve dan reject

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,14 +1,16 @@
 class DataSource {
     
     static searchClub(keyword) {
-        const filteredClubs = clubs.filter(club => {
-            return club.name.toUpperCase().includes(keyword.toUpperCase());
-        });
-    
-        if (filteredClubs.length) {
-            this.onSuccess(filteredClubs);
-        } else {
-            this.onFailed(`${keyword} is not found`);
-        }
+        return new Promise((resolve, reject)=> {
+            const filteredClubs = clubs.filter(club => {
+                return club.name.toUpperCase().includes(keyword.toUpperCase());
+            });
+        
+            if (filteredClubs.length) {
+                resolve(filteredClubs);
+            } else {
+                reject(`${keyword} is not found`);
+            }
+        })
     }
 }
