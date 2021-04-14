fd253256c893d2fd4db9399f6bd5be44f7a532da
adammb86 <adammbachtiar@gmail.com>
2021-04-14 13:23:16 +0700

    Menambahkan import sesuai kebutuhan

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,3 +1,5 @@
+import DataSource from '../data/data-source.js';
+
 const main = () => {
     const searchElement = document.querySelector("#searchElement");
     const buttonSearchElement = document.querySelector("#searchButtonElement");
