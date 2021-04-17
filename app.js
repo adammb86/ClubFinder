0739f076f1193f13934124858d76b9440bd0ca6e
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2021-04-17 15:09:14 +0700

    Menerapkan shadow DOM di app-bar

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import "./src/script/component/app-bar.js";
 import "./src/script/component/search-bar.js"
-import "./src/script/component/club-item.js";
-import "./src/script/component/club-list.js";
 import main from './src/script/view/main.js';
 
 document.addEventListener("DOMContentLoaded", main);
