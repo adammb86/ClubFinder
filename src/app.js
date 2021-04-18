4b1f36e06d1640048e9d0b2828d55ab0c7abcb29
Adam Mukharil Bachtiar <adammbachtiar@gmail.com>
2021-04-18 14:31:12 +0700

    Menambahkan import style.css (lupa)

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import 'regenerator-runtime'
 import "./script/component/app-bar.js";
 import "./script/component/search-bar.js"
 import main from './script/view/main.js';
+import "./styles/style.css"
 
 document.addEventListener("DOMContentLoaded", main);
